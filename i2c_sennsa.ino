0ade160093ddb69a8d7d8e26619359bede5c0ec9
kiruha <rx81laml0080@gmail.com>
2015-08-22 22:01:36 +0900

    fixed #2

--- i2c_sennsa.ino
+++ i2c_sennsa.ino
@@ -1,9 +1,5 @@
 #include <Wire.h>
 
-<<<<<<< HEAD
-=======
-#define I2C_READ_LENGTH 1
->>>>>>> 2a0d11c8df12158e590e979bde933ffba3259f34
 
 void setup() {
   // put your setup code here, to run once:
@@ -13,20 +9,13 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Wire.requestFrom(8,I2C_READ_LENGTH);
-  char i2cBuf[I2C_READ_LENGTH]
+  Wire.requestFrom(8,1);
 
-<<<<<<< HEAD
 
   while(Wire.available()){
       char c =Wire.read();
       Serial.print(c,HEX);
     }
 
-=======
-  for(int i=0;i<I2C_READ_LENGTH;i++){
-    i2cBuf[i]=Wire.read();
-  }
->>>>>>> 2a0d11c8df12158e590e979bde933ffba3259f34
   delay(500);
 } 
