2a8979390e8a8a0b520ddc3861de1e1bcce967e3
kiruha <rx81laml0080@gmail.com>
2015-08-22 19:03:50 +0900

    i2c_ssennsa.inoを追加

---
+++ i2c_sennsa.ino
@@ -0,0 +1,17 @@
+#include <Wire.h>
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Wire.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Wire.requestFrom(8,1);
+
+  while(Wire.available()){
+      char c =Wire.read();
+      Serial.print(c);
+    }
+  delay(500);
+} 
